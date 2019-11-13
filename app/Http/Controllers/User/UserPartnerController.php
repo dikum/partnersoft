7fce00e6595934c391e159237e1f795a81088dce
dikum <amukida@gmail.com>
2019-11-13 17:09:46 +0000

    Get Partners user created

---
+++ app/Http/Controllers/User/UserPartnerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\ApiBaseController;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserPartnerController extends ApiBaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(User $user)
+    {
+        $partners = $user->partners;
+
+        return $this->showAll($partners);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Http\Request;
 Route::resource('users', 'User\UserController', ['except' => ['edit', 'create']]);
 Route::resource('users.payments', 'User\UserPaymentController', ['only' => ['index']]);
 Route::resource('users.comments', 'User\UserCommentController', ['only' => ['index']]);
+Route::resource('users.partners', 'User\UserPartnerController', ['only' => ['index']]);
 
 /**
 *BankStatement
