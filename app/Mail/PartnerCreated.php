780f7a277f1e96c9acc6f5080556e547a9fdc527
dikum <amukida@gmail.com>
2019-11-15 19:16:35 +0000

    Updating Partner Created Mailable

--- app/Mail/PartnerCreated.php
+++ app/Mail/PartnerCreated.php
@@ -2,23 +2,26 @@
 
 namespace App\Mail;
 
+use App\Partner;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class PartnerCreated extends Mailable
 {
     use Queueable, SerializesModels;
+
+    public $partner;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Partner $partner)
     {
-        //
+        $this->partner = $partner;
     }
 
     /**
@@ -28,6 +31,7 @@ class PartnerCreated extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        //return $this->view('view.name');
+        return $this->text('emails.welcome_partner')->with(['partner' => $partner]);
     }
 }
