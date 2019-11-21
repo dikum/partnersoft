d45913dc1a389ad3e4b5b9cb85f800a16614223f
dikum <amukida@gmail.com>
2019-11-21 12:40:26 +0000

    Create EmailTransformer.php

---
+++ app/Transformers/EmailTransformer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class EmailTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Email $email)
+    {
+        return [
+            'userIdentifier' => (int)$email->id,
+            'partnerIdentifier' => (int)$email->partner_id,
+            'enteredByUser' => (int)$email->user_id,
+            'fromEmail' => (string)$email->sender,
+            'toEmail' => (string)$email->recipient,
+            'emailSubject'=>(string)$email->subject,
+            'messageBody' => (string)$email->message,
+            'messageStatus' => (string)$email->status,
+            'createdDate' => $email->created_at,
+            'changeDate' => $email->updated_at,
+        ];
+    }
+}
