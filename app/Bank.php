71c0379d4b46ba5f0dda79139865c80afcbb4272
dikum <amukida@gmail.com>
2019-11-29 15:31:39 +0000

    Update Bank.php
    Include the uuid traits

--- app/Bank.php
+++ app/Bank.php
@@ -4,9 +4,12 @@ namespace App;
 
 use App\BankStatement;
 use Illuminate\Database\Eloquent\Model;
+use Ryancco\HasUuidRouteKey\HasUuidRouteKey;
 
 class Bank extends Model
 {
+	use Traits\UsesUuid;
+
     protected $fillable = [
     	'bank',
     	'bank_code',
