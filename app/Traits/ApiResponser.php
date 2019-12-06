3784ba62ecfaa57d1c9b3ca2ffbd5980ea9a2857
dikum <amukida@gmail.com>
2019-12-06 22:20:46 +0000

    Paginating results

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,7 +3,9 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -28,6 +30,8 @@ trait ApiResponser
 		$collection = $this->filterData($collection, $transformer);
 
 		$collection = $this->sortData($collection, $transformer);
+
+		$collection = $this->paginate($collection);
 
 		$collection = $this->transformData($collection, $transformer);
 
@@ -73,6 +77,32 @@ trait ApiResponser
 		}
 
 		return $collection;
+	}
+
+	protected function paginate(Collection $collection)
+	{
+		$rules = [
+			'per_page' => 'integer|min:2|max:100',
+		];
+
+		Validator::validate(request()->all(), $rules);
+
+		$page = LengthAwarePaginator::resolveCurrentPage();
+
+		$perPage = 50;
+
+		if(request()->has('per_page'))
+			$perPage = (int)request()->per_page;
+
+		$result = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+		$paginated = new LengthAwarePaginator($result, $collection->count(), $perPage, $page, [
+			'path' => LengthAwarePaginator::resolveCurrentPath(),
+		]);
+
+		$paginated->appends(request()->all());
+
+		return $paginated;
 	}
 
 	protected function transformData($data, $transformer)
