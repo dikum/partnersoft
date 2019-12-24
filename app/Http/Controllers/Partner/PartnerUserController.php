78265570cf9e2a2ac51f725fbd47f7dad9da6355
dikum <amukida@gmail.com>
2019-12-24 23:06:24 +0000

    Create PartnerUserController.php
    Since partner is a user, this controller shows the user related to a partner.

---
+++ app/Http/Controllers/Partner/PartnerUserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Partner;
+
+use App\Http\Controllers\ApiBaseController;
+use App\Http\Controllers\Controller;
+use App\Partner;
+use App\User;
+use Illuminate\Http\Request;
+
+class PartnerUserController extends ApiBaseController
+{
+	/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Partner $partner)
+    {
+        $user = User::where('user_id', $partner->user_id)->get();
+
+        return $this->showAll($user);
+    }
+}
