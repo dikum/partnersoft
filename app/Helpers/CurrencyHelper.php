e719aa6237958abcb588dd43f7910db4ec4f6931
dikum <amukida@gmail.com>
2020-03-07 15:36:37 +0000

    Create CurrencyHelper.php

---
+++ app/Helpers/CurrencyHelper.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\DB;
+
+	
+class CurrencyHelper
+{
+	public static function get_currency_name($currency_id)
+	{
+		$currency_name = DB::table('curremcies')
+			->select('currency')
+			->where('currency_id', '=', $currency_id)
+			->value('currency_id');
+
+		return $currency_name;
+	}
+}
+
+?>
