23400057ccea1f03efa2a998d9c5b4a84c27635e
dikum <amukida@gmail.com>
2020-03-20 16:04:35 +0000

    Update CurrencyHelper.php
    Wrong spelling of currencies corrected

--- app/Helpers/CurrencyHelper.php
+++ app/Helpers/CurrencyHelper.php
@@ -9,7 +9,7 @@ class CurrencyHelper
 {
 	public static function get_currency_name($currency_id)
 	{
-		$currency_name = DB::table('curremcies')
+		$currency_name = DB::table('currencies')
 			->select('currency')
 			->where('currency_id', '=', $currency_id)
 			->value('currency_id');
