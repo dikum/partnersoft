4359dcf3448a269cfe7100338481d35e3ce6eb27
dikum <amukida@gmail.com>
2020-03-26 13:01:18 +0000

    Update PartnerPaymentController.php

--- app/Http/Controllers/Partner/PartnerPaymentController.php
+++ app/Http/Controllers/Partner/PartnerPaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Partner;
 use App\Http\Controllers\ApiBaseController;
 use App\Http\Controllers\Controller;
 use App\Partner;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\dd;
@@ -16,8 +17,10 @@ class PartnerPaymentController extends ApiBaseController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Partner $partner)
+    public function index($partner_id)
     {
+        $partner = User::where('user_id', $partner_id)->findOrFail();
+        
         $payments = $partner->payments()->with('bank_statement')->get();
         
         return $this->showAll($payments);
