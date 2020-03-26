0f8aaca11b2a0e7e296db38b3b20df392138054e
dikum <amukida@gmail.com>
2020-03-26 13:01:06 +0000

    Update PartnerCommentController.php

--- app/Http/Controllers/Partner/PartnerCommentController.php
+++ app/Http/Controllers/Partner/PartnerCommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Partner;
 use App\Http\Controllers\ApiBaseController;
 use App\Http\Controllers\Controller;
 use App\Partner;
+use App\User;
 use Illuminate\Http\Request;
 
 class PartnerCommentController extends ApiBaseController
@@ -14,8 +15,10 @@ class PartnerCommentController extends ApiBaseController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Partner $partner)
+    public function index($partner_id)
     {
+
+        $partner = User::where('user_id', $partner_id)->findOrFail();
         $comments = $partner->partner_comments;
 
         return $this->showAll($comments);
