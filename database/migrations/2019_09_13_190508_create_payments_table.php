06f1dc36bb2f6567309f5735724ae81c7647fe90
dikum <amukida@gmail.com>
2020-03-26 16:37:40 +0000

    Update 2019_09_13_190508_create_payments_table.php

--- database/migrations/2019_09_13_190508_create_payments_table.php
+++ database/migrations/2019_09_13_190508_create_payments_table.php
@@ -15,14 +15,14 @@ class CreatePaymentsTable extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->uuid('payment_id')->primary();
-            $table->string('partner_id');
+            $table->string('made_by');
             $table->string('bank_statement_id');
-            $table->string('user_id')->nullable();
+            $table->string('entered_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('partner_id')->references('partner_uuid')->on('partners');
-            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->foreign('made_by')->references('user_id')->on('users');
+            $table->foreign('entered_by')->references('user_id')->on('users');
             $table->foreign('bank_statement_id')->references('bank_statement_id')->on('bank_statements');
     
         });
