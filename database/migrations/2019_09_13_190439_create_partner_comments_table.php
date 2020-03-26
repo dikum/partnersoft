e0cab0d759d22fc6c4e51ee65b72e3de97f6479a
dikum <amukida@gmail.com>
2020-03-26 16:37:37 +0000

    Update 2019_09_13_190439_create_partner_comments_table.php

--- database/migrations/2019_09_13_190439_create_partner_comments_table.php
+++ database/migrations/2019_09_13_190439_create_partner_comments_table.php
@@ -15,14 +15,14 @@ class CreatePartnerCommentsTable extends Migration
     {
         Schema::create('partner_comments', function (Blueprint $table) {
             $table->uuid('comment_id')->primary();
-            $table->string('partner_id');
-            $table->string('user_id');
+            $table->string('made_by');
+            $table->string('to');
             $table->text('comment');
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('partner_id')->references('partner_uuid')->on('partners');
-            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->foreign('made_by')->references('user_id')->on('users');
+            $table->foreign('to')->references('user_id')->on('users');
         });
     }
 
