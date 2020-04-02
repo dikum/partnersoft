202d287d65bb55a98e8e519a231b246118c42ea5
dikum <amukida@gmail.com>
2020-04-02 18:39:10 +0100

    Update ContinentController.php

--- app/Http/Controllers/Continent/ContinentController.php
+++ app/Http/Controllers/Continent/ContinentController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class ContinentController extends ApiBaseController
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
     /**
      * Display a listing of the resource.
      *
