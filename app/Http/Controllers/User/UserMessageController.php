7303abc9393c8c798651ad20fae94385111cd0c3
dikum <amukida@gmail.com>
2020-04-02 18:40:40 +0100

    Update UserMessageController.php

--- app/Http/Controllers/User/UserMessageController.php
+++ app/Http/Controllers/User/UserMessageController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 
 class UserMessageController extends ApiBaseController
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     /**
      * Display a listing of the resource.
      *
