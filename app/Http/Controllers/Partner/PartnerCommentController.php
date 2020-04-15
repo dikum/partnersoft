a6190d76cd7a2bc24615931792150955cc7e105f
dikum <amukida@gmail.com>
2020-04-15 22:04:59 +0100

    Update PartnerCommentController.php

--- app/Http/Controllers/Partner/PartnerCommentController.php
+++ app/Http/Controllers/Partner/PartnerCommentController.php
@@ -13,7 +13,8 @@ class PartnerCommentController extends ApiBaseController
 
     public function __construct()
     {
-        parent::__construct();
+
+        $this->middleware('auth:api');
 
         $this->middleware('transform.input:' . BankTransformer::class)->only(['store', 'update']);
     }
