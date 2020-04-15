1cafd3fef552ce1f507ecab9626ac4c25e002e77
dikum <amukida@gmail.com>
2020-04-15 22:03:16 +0100

    Update BankController.php
    Protect the Controller

--- app/Http/Controllers/Bank/BankController.php
+++ app/Http/Controllers/Bank/BankController.php
@@ -13,7 +13,7 @@ class BankController extends ApiBaseController
 
     public function __construct()
     {
-        parent::__construct();
+        $this->middleware('auth:api');
 
         $this->middleware('transform.input:' . BankTransformer::class)->only(['store', 'update']);
     }
