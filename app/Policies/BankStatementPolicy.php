bb7bd0a54f2e2c422a465c4cc8c2448bfb1dd124
dikum <amukida@gmail.com>
2020-04-15 23:32:35 +0100

    Create BankStatementPolicy.php

---
+++ app/Policies/BankStatementPolicy.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Policies;
+
+use App\BankStatement;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BankStatementPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any bank statements.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the bank statement.
+     *
+     * @param  \App\User  $user
+     * @param  \App\BankStatement  $bankStatement
+     * @return mixed
+     */
+    public function view(User $user, BankStatement $bankStatement)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create bank statements.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the bank statement.
+     *
+     * @param  \App\User  $user
+     * @param  \App\BankStatement  $bankStatement
+     * @return mixed
+     */
+    public function update(User $user, BankStatement $bankStatement)
+    {
+        if($user->isAdmin())
+            return true;
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the bank statement.
+     *
+     * @param  \App\User  $user
+     * @param  \App\BankStatement  $bankStatement
+     * @return mixed
+     */
+    public function delete(User $user, BankStatement $bankStatement)
+    {
+        if($user->isAdmin())
+            return true;
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the bank statement.
+     *
+     * @param  \App\User  $user
+     * @param  \App\BankStatement  $bankStatement
+     * @return mixed
+     */
+    public function restore(User $user, BankStatement $bankStatement)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the bank statement.
+     *
+     * @param  \App\User  $user
+     * @param  \App\BankStatement  $bankStatement
+     * @return mixed
+     */
+    public function forceDelete(User $user, BankStatement $bankStatement)
+    {
+        //
+    }
+}
