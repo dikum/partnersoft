324e858771872f2d9d7ca2111997830ae92d77c4
dikum <amukida@gmail.com>
2020-04-16 13:13:47 +0100

    Create CountryPolicy.php

---
+++ app/Policies/CountryPolicy.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Policies;
+
+use App\Country;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CountryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any countries.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        if($user->isAdmin() || $user->isRegularUser() || $user->isPartner()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the country.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Country  $country
+     * @return mixed
+     */
+    public function view(User $user, Country $country)
+    {
+        if($user->isAdmin() || $user->isRegularUser()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create countries.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the country.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Country  $country
+     * @return mixed
+     */
+    public function update(User $user, Country $country)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the country.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Country  $country
+     * @return mixed
+     */
+    public function delete(User $user, Country $country)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can restore the country.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Country  $country
+     * @return mixed
+     */
+    public function restore(User $user, Country $country)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the country.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Country  $country
+     * @return mixed
+     */
+    public function forceDelete(User $user, Country $country)
+    {
+        //
+    }
+
+    public function before($user, $ability)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+}
