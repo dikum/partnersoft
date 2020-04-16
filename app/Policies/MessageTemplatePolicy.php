00f2aaa68b531d4c186077ce4a962a3a6e55870d
dikum <amukida@gmail.com>
2020-04-16 13:14:01 +0100

    Create MessageTemplatePolicy.php

---
+++ app/Policies/MessageTemplatePolicy.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Policies;
+
+use App\MessageTemplate;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MessageTemplatePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any message templates.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+    }
+
+    /**
+     * Determine whether the user can view the message template.
+     *
+     * @param  \App\User  $user
+     * @param  \App\MessageTemplate  $messageTemplate
+     * @return mixed
+     */
+    public function view(User $user, MessageTemplate $messageTemplate)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+    }
+
+    /**
+     * Determine whether the user can create message templates.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+    }
+
+    /**
+     * Determine whether the user can update the message template.
+     *
+     * @param  \App\User  $user
+     * @param  \App\MessageTemplate  $messageTemplate
+     * @return mixed
+     */
+    public function update(User $user, MessageTemplate $messageTemplate)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+    }
+
+    /**
+     * Determine whether the user can delete the message template.
+     *
+     * @param  \App\User  $user
+     * @param  \App\MessageTemplate  $messageTemplate
+     * @return mixed
+     */
+    public function delete(User $user, MessageTemplate $messageTemplate)
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can restore the message template.
+     *
+     * @param  \App\User  $user
+     * @param  \App\MessageTemplate  $messageTemplate
+     * @return mixed
+     */
+    public function restore(User $user, MessageTemplate $messageTemplate)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the message template.
+     *
+     * @param  \App\User  $user
+     * @param  \App\MessageTemplate  $messageTemplate
+     * @return mixed
+     */
+    public function forceDelete(User $user, MessageTemplate $messageTemplate)
+    {
+        //
+    }
+}
