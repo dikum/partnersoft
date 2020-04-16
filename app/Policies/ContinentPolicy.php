6c3813cf5e4e9e230d493ef36958a8c078120f4a
dikum <amukida@gmail.com>
2020-04-16 13:13:42 +0100

    Create ContinentPolicy.php

---
+++ app/Policies/ContinentPolicy.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Policies;
+
+use App\Continent;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ContinentPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any continents.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        if($user->isAdmin() || $user->isRegularUser()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the continent.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Continent  $continent
+     * @return mixed
+     */
+    public function view(User $user, Continent $continent)
+    {
+        if($user->isAdmin() || $user->isRegularUser()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create continents.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the continent.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Continent  $continent
+     * @return mixed
+     */
+    public function update(User $user, Continent $continent)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the continent.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Continent  $continent
+     * @return mixed
+     */
+    public function delete(User $user, Continent $continent)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can restore the continent.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Continent  $continent
+     * @return mixed
+     */
+    public function restore(User $user, Continent $continent)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the continent.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Continent  $continent
+     * @return mixed
+     */
+    public function forceDelete(User $user, Continent $continent)
+    {
+        //
+    }
+
+    public function before($user, $ability)
+    {
+        if($user->isAdmin()) {
+        return true;
+    }
+}
