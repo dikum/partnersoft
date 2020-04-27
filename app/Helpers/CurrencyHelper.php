0bd9f7365d56a8dca8b5ca7505bfd5681c899214
dikum <amukida@gmail.com>
2020-04-27 19:41:19 +0100

    Update CurrencyHelper.php

--- app/Helpers/CurrencyHelper.php
+++ app/Helpers/CurrencyHelper.php
@@ -7,14 +7,25 @@ use Illuminate\Support\Facades\DB;
 	
 class CurrencyHelper
 {
-	public static function get_currency_name($currency_id)
+	public static function get_currency_code($currency_id)
 	{
-		$currency_name = DB::table('currencies')
-			->select('currency')
+		$currency_code = DB::table('currencies')
+			->select('currency_code')
 			->where('currency_id', '=', $currency_id)
+			->value('currency_code');
+
+		return $currency_code;
+	}
+
+
+	public static function get_currency_id_with_code($currency_code)
+	{
+		$currency_id = DB::table('currencies')
+			->select('currency_id')
+			->where('currency_code', '=', $currency_code)
 			->value('currency_id');
 
-		return $currency_name;
+		return $currency_id;
 	}
 }
 
