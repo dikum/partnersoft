24a8f9b986586935f59586af193fc8ef3492683c
dikum <amukida@gmail.com>
2020-04-27 19:46:51 +0100

    Create PaymentPolicy.php

---
+++ app/Policies/PaymentPolicy.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Policies;
+
+use App\Payment;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PaymentPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any payments.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the payment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Payment  $payment
+     * @return mixed
+     */
+    public function view(User $user, Payment $payment)
+    {
+        if($user->isAdmin() || $user->isRegularUser() || $user->user_id == $payment->made_by)
+            return true;
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create payments.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the payment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Payment  $payment
+     * @return mixed
+     */
+    public function update(User $user, Payment $payment)
+    {
+        if($user->isAdmin() || $user->isRegularUser())
+            return true;
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the payment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Payment  $payment
+     * @return mixed
+     */
+    public function delete(User $user, Payment $payment)
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can restore the payment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Payment  $payment
+     * @return mixed
+     */
+    public function restore(User $user, Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the payment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Payment  $payment
+     * @return mixed
+     */
+    public function forceDelete(User $user, Payment $payment)
+    {
+        //
+    }
+
+     public function before($user, $ability)
+    {
+        return $user->isAdmin();
+    }
+}
