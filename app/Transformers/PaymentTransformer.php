e665522cf33ea3290dc41c07becd1a41b95c22df
dikum <amukida@gmail.com>
2020-08-09 23:38:39 +0100

    Update PaymentTransformer.php

--- app/Transformers/PaymentTransformer.php
+++ app/Transformers/PaymentTransformer.php
@@ -25,7 +25,23 @@ class PaymentTransformer extends TransformerAbstract
             'userIdentifier' => (string)$payment->entered_by,
             'createdDate' => (string)$payment->created_at,
             'changeDate' => (string)$payment->updated_at,
-            'bank_statement' => $paymentBankStatement->index($payment),
+            //'bank_statement' => $paymentBankStatement->index($payment),
+            //'bank_statement' => $payment->bank_statement,
+            'transactionIdenitifier' => $payment->bank_statement->transaction_id,
+            'bankIdentifier' => $payment->bank_statement->bank_id,
+            'currencyIdentifier' => $payment->bank_statement->currency_id,
+            'friendlyPartnerIdentifier' => $payment->bank_statement->partner_id,
+            'depositor' => $payment->bank_statement->depositor,
+            'paymentDescription' => $payment->bank_statement->description,
+            'amountPaid' => $payment->bank_statement->amount,
+            'datePaid' => $payment->bank_statement->payment_date,
+            'valueDate' => $payment->bank_statement->value_date,
+            'paymentChannel' => $payment->bank_statement->payment_channel,
+            'emailAddress' => $payment->bank_statement->email,
+            'phoneNumber' => $payment->bank_statement->phone,
+            'bankStatementCreatedDate' => $payment->bank_statement->created_at,
+            'bankStatementUpdatedDate' => $payment->bank_statement->updated_at,
+
 
 
             'links' => [
@@ -47,6 +63,20 @@ class PaymentTransformer extends TransformerAbstract
             'userIdentifier' => 'user_id',
             'createdDate' => 'created_at',
             'changeDate' => 'updated_at',
+            'transactionIdenitifier' => 'transaction_id',
+            'bankIdentifier' => 'bank_id',
+            'currencyIdentifier' => 'currency_id',
+            'friendlyPartnerIdentifier' => 'partner_id',
+            'depositor' => 'depositor',
+            'paymentDescription' => 'description',
+            'amountPaid' => 'amount',
+            'datePaid' => 'payment_date',
+            'valueDate' => 'value_date',
+            'paymentChannel' => 'payment_channel',
+            'emailAddress' => 'email',
+            'phoneNumber' => 'phone',
+            'bankStatementCreatedDate' => 'created_at',
+            'bankStatementUpdatedDate' => 'updated_at'
 
         ];
 
@@ -63,7 +93,21 @@ class PaymentTransformer extends TransformerAbstract
             'user_id' => 'userIdentifier',
             'created_at' => 'createdDate',
             'updated_at' => 'changeDate',
-
+            'transaction_id' => 'transactionIdenitifier',
+            'bank_id' => 'bankIdentifier',
+            'currency_id' => 'currencyIdentifier',
+            'partner_id' => 'friendlyPartnerIdentifier',
+            'depositor' => 'depositor',
+            'description' => 'paymentDescription',
+            'amount' => 'amountPaid',
+            'payment_date' => 'datePaid',
+            'value_date' => 'valueDate',
+            'payment_channel' => 'paymentChannel',
+            'email' => 'emailAddress',
+            'phone' => 'phoneNumber',
+            'created_at' => 'bankStatementCreatedDate',
+            'updated_at' => 'bankStatementUpdatedDate'
+            
         ];
 
         return isset($attributes[$index]) ? $attributes[$index] : null;
